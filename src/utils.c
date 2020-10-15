a35ca48a7531b5f2a1c89bdf41b65788aa6548ea
Azim Muradov <azim.muradov.dev@gmail.com>
2020-10-15 18:22:47 +0300

    Simplify program

--- src/utils.c
+++ src/utils.c
@@ -6,16 +6,8 @@
 
 #include "utils.h"
 
-int fprintf_array_ll(FILE *stream, long long *array, size_t array_size) {
-    if (array_size > 0) {
-        if (fprintf(stream, "%lld", array[0]) < 0) return -1;
+void fprintf_array_ll(FILE *stream, long long *array, size_t array_size) {
+    for (size_t i = 0; i < array_size; ++i) {
+        fprintf(stream, "%lld ", array[i]);
     }
-    for (size_t i = 1; i < array_size; ++i) {
-        if (fprintf(stream, " %lld", array[i]) < 0) return -1;
-    }
-    if (array_size > 0) {
-        if (fprintf(stream, "\n") < 0) return -1;
-    }
-
-    return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -9,8 +9,6 @@
 
 #include <stdio.h>
 
-#define errprintf(...) fprintf(stderr, __VA_ARGS__)
-
-int fprintf_array_ll(FILE *stream, long long *array, size_t array_size);
+void fprintf_array_ll(FILE *stream, long long *array, size_t array_size);
 
 #endif // TASK_2_UTILS_H
